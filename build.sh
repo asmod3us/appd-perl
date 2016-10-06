ad039d0ea20aba896f150bcdd717228238a69f04
Scimon <simon.proctor@gmail.com>
2016-10-06 12:22:52 +0100

    Build script copies modules to local perl

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@
 rm *.o *.so *wrap.c *.pm > /dev/null 2>&1
 
 #get flags used to compile Perl
-PERL_INC=$(perl -e 'use Config; print "$Config{archlib}\n";')/CORE
+PERL_INC=$(perl -e 'use Config; print "$Config{archlib}\n";')
 CFLAGS=$(perl -MConfig -e "print \$Config{ccflags}")
-CFLAGS="-fPIC -I $APPD_SDK -I$PERL_INC $CFLAGS"
+CFLAGS="-fPIC -I $APPD_SDK -I$PERL_INC/CORE $CFLAGS"
 
 # swig generates the wrapper code
 swig -perl5 appd.i
@@ -20,3 +20,4 @@ gcc $CFLAGS -c proxy.c appd_wrap.c
 echo "Linking..."
 ld -G -L $APPD_SDK/lib proxy.o appd_wrap.o -o appd.so -lappdynamics_native_sdk
 
+cp appd.pm appd.so $PERL_INC
